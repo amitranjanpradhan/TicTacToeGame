3d8f540a2eb71eaabb7cd5bcaefe47c92ef2ffa6
amitranjan <amitranjanpradhan456@gmail.com>
2021-05-23 22:58:16 +0530

    [Amit] Added Display Board_UC3

--- src/com/tictactoe/TicTacToeGame.java
+++ src/com/tictactoe/TicTacToeGame.java
@@ -3,7 +3,8 @@ package com.tictactoe;
 import java.util.Scanner;
 public class TicTacToeGame {
 	private char board[]=new char[10]; //created a tictactoe board of size 10
-    private String usersymbol,computersymbol;
+    String usersymbol;
+	String computersymbol;
     TicTacToeGame(){
 
         createboard(); //inside the constructor am calling createboard method
@@ -16,21 +17,38 @@ public class TicTacToeGame {
     }
     public void takeUserInput(){ //created a method named
         Scanner Sc=new Scanner(System.in);
-        System.out.println("Your turn: ");
+        System.out.println("Choose Your Letter x or o : ");
         String symbol=Sc.next(); // taking user input to check
         if (symbol=="x"){        //Comparing the user input
             usersymbol= "x";
             computersymbol="o";
+            System.exit(0);
         }else if (symbol=="o"){
             usersymbol="o";
             computersymbol="x";
+            System.exit(0);
         }else{
             System.out.println("invalid option");
         }
     }
-    public static void main(String[] args) {
-    	System.out.println("Welcome To TicTacToe Game");
-    	TicTacToeGame tictactoegame = new TicTacToeGame();
+    public void showBoard(){
+        String horizontalPart = "+---";
+        String verticalPart = "|   ";
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(horizontalPart);
+            }
+            System.out.print("+\n");
+            for (int j = 0; j < 3; j++) {
+                System.out.print(verticalPart);
+            }
+            System.out.print("+\n");
+        }
     }
-	
+
+    public static void main(String[]args){
+    	System.out.println("Welcome To TicTacToe Game");
+        TicTacToeGame tictactoe=new TicTacToeGame(); //created object for class TicTacToeGame
+        tictactoe.showBoard();
+    }
 }
