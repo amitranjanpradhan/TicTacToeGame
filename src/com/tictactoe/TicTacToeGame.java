7463306e56295032eeae7d579986cbb247588f0c
amitranjan <amitranjanpradhan456@gmail.com>
2021-05-23 22:37:08 +0530

    [Amit] Added User input

--- src/com/tictactoe/TicTacToeGame.java
+++ src/com/tictactoe/TicTacToeGame.java
@@ -1,21 +1,36 @@
 package com.tictactoe;
 
+import java.util.Scanner;
 public class TicTacToeGame {
-	public char board[] = new char[10];//created a tictactoe board of size 10
+	private char board[]=new char[10]; //created a tictactoe board of size 10
+    private String usersymbol,computersymbol;
+    TicTacToeGame(){
 
-    TicTacToeGame() {
-        createboard();   //inside the constructor am calling createboard method
+        createboard(); //inside the constructor am calling createboard method
+        takeUserInput();//inside the constructor am calling takeUserInput method
     }
-
-    public void createboard() { //created a method named createboard.
-        for (int count = 1; count < 10; count++) {
-            board[count] = ' ';
+    public void createboard(){  //created a method named createboard.
+        for(int count=1;count<10;count++){ //it will iterate this forloop from 1 to 10
+            board[count]=' '; //assigned empty space
         }
     }
-
+    public void takeUserInput(){ //created a method named
+        Scanner Sc=new Scanner(System.in);
+        System.out.println("Your turn: ");
+        String symbol=Sc.next(); // taking user input to check
+        if (symbol=="x"){        //Comparing the user input
+            usersymbol= "x";
+            computersymbol="o";
+        }else if (symbol=="o"){
+            usersymbol="o";
+            computersymbol="x";
+        }else{
+            System.out.println("invalid option");
+        }
+    }
     public static void main(String[] args) {
     	System.out.println("Welcome To TicTacToe Game");
     	TicTacToeGame tictactoegame = new TicTacToeGame();
-    	tictactoegame.createboard();
     }
+	
 }
